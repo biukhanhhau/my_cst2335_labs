9a71666ef14498d518425d4aa52103568c0c90c0
biukhanhhau <biu12.02dev@gmail.com>
2026-06-15 14:38:00 -0400

    Lab 4 finished

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:encrypted_shared_preferences/encrypted_shared_preferences.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,9 +11,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Lab4',
       theme: ThemeData(
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
@@ -34,11 +36,33 @@ class _MyHomePageState extends State<MyHomePage> {
 
   var imageSource = "images/question-mark.png";
 
+  final EncryptedSharedPreferences _prefs = EncryptedSharedPreferences();
+
   @override
   void initState() {
     super.initState();
     _loginController = TextEditingController();
     _passwordController = TextEditingController();
+    _loadSavedData();
+  }
+
+  Future<void> _loadSavedData() async {
+    String savedLogin = await _prefs.getString('login_name');
+    String savedPassword = await _prefs.getString('password');
+
+    if (savedLogin.isNotEmpty && savedPassword.isNotEmpty) {
+      setState(() {
+        _loginController.text = savedLogin;
+        _passwordController.text = savedPassword;
+      });
+
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(
+            content: Text('Previous login info reloaded successfully!'),
+            duration: Duration(seconds: 2),
+          ),
+        );
+    }
   }
 
   @override
@@ -56,57 +80,88 @@ class _MyHomePageState extends State<MyHomePage> {
         imageSource = "images/stop-sign.png";
       }
     });
+
+    _showLoginDialog();
+  }
+
+  void _showLoginDialog() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Save Login Info?'),
+          content: const Text('Would you like to save your username and password?'),
+          actions: [
+            TextButton(
+              onPressed: () async {
+                Navigator.pop(context);
+                await _prefs.clear();
+              },
+              child: const Text('No'),
+            ),
+            TextButton(
+              onPressed: () async {
+                Navigator.pop(context);
+                await _prefs.setString('login_name', _loginController.text);
+                await _prefs.setString('password', _passwordController.text);
+              },
+              child: const Text('Yes'),
+            ),
+          ],
+        );
+      },
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-   return Scaffold(
+    return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
       body: Center(
         child: Column(
-          mainAxisAlignment: .center,
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-              Padding(
-                padding: EdgeInsets.all(8.0),
-                child: TextField(
-                  controller: _loginController,
-                  decoration: InputDecoration(
-                      labelText: "Login name"
-                  ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextField(
+                controller: _loginController,
+                decoration: const InputDecoration(
+                    labelText: "Login name"
                 ),
               ),
+            ),
 
-              Padding(
-                padding: EdgeInsets.all(8.0),
-                child: TextField(
-                  controller: _passwordController,
-                  obscureText: true,
-                  decoration: InputDecoration(
-                      labelText: "password"
-                  ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextField(
+                controller: _passwordController,
+                obscureText: true,
+                decoration: const InputDecoration(
+                    labelText: "password"
                 ),
               ),
+            ),
 
-              Padding(
-                padding: EdgeInsets.all(8.0),
-                child: ElevatedButton(
-                  onPressed: buttonClicked,
-                  child: Text("Login"),
-                ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ElevatedButton(
+                onPressed: buttonClicked,
+                child: const Text("Login"),
               ),
+            ),
 
-              Semantics(
-                label: 'Image for responding login',
-                child: Image.asset(
-                  imageSource,
-                  width: 300,
-                  height: 300,
-                ),
+            Semantics(
+              label: 'Image for responding login',
+              child: Image.asset(
+                imageSource,
+                width: 300,
+                height: 300,
               ),
-            ],
+            ),
+          ],
         ),
       ),
     );
